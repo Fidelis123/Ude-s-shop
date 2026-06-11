e6280ada8a9f129fd18dc80963ac189c07a506c9
Fidelis Udenwa <fiojinkmama2020@gmail.com>
2026-06-10 18:15:13 -0700

    half way navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import { IoMdSearch } from 'react-icons/io'
 
 const Navbar = () => {
   return (
-    <div >
+    <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white z-40" >
         {/* Upper Navbar */}
-        <div>
-            <div>
+        <div className="bg-primary/40 ">
+            <div className="container justify-between items-center flex">
                 <div>
-                    <a href="#">
-                        <img src={logo} alt="Logo" />
+                    <a href="#"
+                    className="font-bold text-2xl  sm:text-3xl flex items-center gap-2">
+                        <img src={logo} alt="Logo" 
+                        className="w-10 rounded-full"/>
+                        Ude's shop
                     </a>
                 </div>
                 {/* search bar and order button */}
                 <div>
-                  <div className="group">
+                  <div className="group relative">
                     <input type="text" 
                         placeholder="Search..." 
-                        className="w-50 sm:w-50 group-hover:w-75 transition-all duration-300
-                        rounded-full border border-color-secondary:var(--color-secondary)
-                        px-2 py-1 focus:outline-none focus:border focus:border-secondary" />
+                        className="w-50 sm:w-64 group-hover:w-75 transition-all duration-300
+                        rounded-full border border-secondary
+                        px-2 py-1 focus:outline-none focus:border focus:border-secondary bg-white text-primary placeholder:text-gray-400" />
+                        <IoMdSearch className="text-primary text-xl" />
                   </div>
                 </div>
             </div> 
